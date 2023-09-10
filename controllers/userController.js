5d08f0e00f048e5bef86d368c8460cd6b8724a53
Sayak Ghosh <sayakmemari@gmail.com>
2023-09-10 18:56:13 +0530

    Update User API Route

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,4 @@
+import postModel from "../models/postModel.js";
 import userModel from "../models/userModel.js";
 
 export const userController = async (req, res) => {
@@ -7,16 +8,26 @@ export const userController = async (req, res) => {
 
     const numberOfFollowers = currentUser.followers.length;
     const numberOfFollowing = currentUser.following.length;
+    const numberOfPosts = currentUser.posts.length;
+
+    const posts = await Promise.all(
+      currentUser.posts.map(async (post) => {
+        const getPost = await postModel.findOne({ _id: post._id });
+        return getPost;
+      })
+    );
 
     return res.status(200).send({
-        success: true,
-        message: "Successfully retrieved the user details",
-        user : {
-            "User Name" : currentUser.name,
-            "No. of followers" : numberOfFollowers,
-            "No. of following" : numberOfFollowing,
-        }
-    })
+      success: true,
+      message: "Successfully retrieved the user details",
+      user: {
+        "User Name": currentUser.name,
+        "No. of followers": numberOfFollowers,
+        "No. of following": numberOfFollowing,
+        "No. of posts": numberOfPosts,
+        Posts: posts,
+      },
+    });;
   } catch (err) {
     res.status(500).send({
       success: false,
